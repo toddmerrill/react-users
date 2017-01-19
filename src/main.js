10aeff31268dd4c556e64930423902a3d0a5089d
Todd Merrill <github@toddmerrill.com>
2017-01-19 13:58:18 +0100

    added bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Users from './users.js';
+
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import './style/users.css';
 
 render(<Users />, document.querySelector('#app'));

52b5d55afba25a4b1a69dd64aa3a1ff24f436ef8
Todd Merrill <github@toddmerrill.com>
2017-01-20 18:34:00 +0100

    added mocked up user list

--- src/users.js
+++ src/users.js
@@ -1,8 +1,47 @@
 import React from 'react'
 
+const userList = {
+    "users": [
+        {
+            "userId": "vrkkq9leh",
+            "firstName": "Jane",
+            "lastName": "Derane",
+            "age": "111"
+        }, {
+            "userId": "qsgg0uv84",
+            "firstName": "Kavita",
+            "lastName": "Blalita",
+            "age": "8"
+        }, {
+            "userId": "oi7i7lpte",
+            "firstName": "Johnny",
+            "lastName": "Blow",
+            "age": "5455"
+        }, {
+            "userId": "k49e0vv9s",
+            "firstName": "Daniel J.",
+            "lastName": "Whaaa?",
+            "age": "999"
+        }, {
+            "userId": "tutadlocy",
+            "firstName": "Joe",
+            "lastName": "Blow",
+            "age": "333"
+        }
+    ]
+}
+
 function User(props) {
   return (
-      <div>{props.user.firstName} {props.user.lastName}</div>
+      <div className="leftlist"><h4>{props.user.firstName} {props.user.lastName}</h4></div>
+  );
+}
+
+
+function UserList(props) {
+  const userList = props.users.map((user, index) => {return <User key={index} user={user}/>});
+  return (
+      <div>{userList}</div>
   );
 }
 
@@ -25,10 +64,17 @@ class Users extends React.Component {
   }
 
   render() {
-    const user = {firstName: "Bob", lastName: "Jones"};
     return (
-        <div>
-            <User user={user} />
+        <div className="col-md-4">
+            <h2>Users
+                <a className="btn btn-circle-plus">
+                    <span className="glyphicon glyphicon-plus-sign"></span>
+                </a>
+                <a className="btn btn-circle-minus">
+                    <span className="glyphicon glyphicon-minus-sign"></span>
+                </a>
+            </h2>
+            <UserList users={userList.users}/>
         </div>
     );
   }

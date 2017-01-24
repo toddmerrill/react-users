4caf9637c1a9a7ea6bfb7f2465407e52e49bdd77
Todd Merrill <github@toddmerrill.com>
2017-01-24 13:27:45 +0100

    added local state management

--- src/users.js
+++ src/users.js
@@ -1,4 +1,5 @@
 import React from 'react'
+const R = require('ramda')
 
 const userList = {
     "users": [
@@ -32,33 +33,36 @@ const userList = {
 }
 
 function User(props) {
+  let rowClass="listrow";
   return (
-      <div className="listrow"><h4>{props.user.firstName} {props.user.lastName}</h4></div>
+      <div className={rowClass} onClick={() => props.onClick(props.user)}><h4>{props.user.firstName} {props.user.lastName}</h4></div>
   );
 }
 
 function UserForm(props) {
   return (
-      <div className="col-md-6 content">
+      <div className="rightList" class="col-md-6 content">
           <h2>Personal Information</h2>
           <form className="form-horizontal">
               <div className="form-group">
-                  <label className="control-label" htmlFor="inputFirstName">First Name</label>
+                  <label className="controlLabel" class="control-label" htmlFor="inputFirstName">First Name</label>
                   <div className="controls">
-                      <input type="text" className="form-control" id="inputFirstName" placeholder="First Name"
-                             autofocus focus="focusInput" tabIndex="0" />
+                      <input name="firstName" type="text" className="form-control" id="inputFirstName" placeholder="First Name"
+                             autofocus focus="focusInput" tabIndex="0" value={props.user.firstName} onChange={props.onChange}/>
                   </div>
               </div>
               <div className="form-group">
                   <label className="control-label" htmlFor="inputLastName">Last Name</label>
                   <div className="controls">
-                      <input type="text" className="form-control" id="inputLastName" placeholder="Last Name" />
+                      <input name="lastName" type="text" className="form-control" id="inputLastName" placeholder="Last Name"
+                      value={props.user.lastName} onChange={props.onChange} />
                   </div>
               </div>
               <div className="form-group">
                   <label className="control-label" htmlFor="inputAge">Age</label>
                   <div className="controls">
-                      <input type="text" className="form-control" id="inputAge" placeholder="Age" />
+                      <input name="age" type="text" className="form-control" id="inputAge" placeholder="Age"
+                      value={props.user.age} onChange={props.onChange}/>
                   </div>
               </div>
           </form>
@@ -83,29 +87,41 @@ function UserHeader(props) {
 }
 
 function UserList(props) {
-    const userList = props.users.map((user, index) => {return <User key={index} user={user}/>});
+    const userList = props.users.map((user, index) => {return <User key={user.userId} user={user} onClick={props.userClick}/>});
     return (
         <div className="listItems">{userList}</div>
     );
 }
 
 const getStartState = () => {
-   return {};
+    const firstUser = userList.users.length > 0 ? userList.users[0] : null;
+   return {
+       users: userList.users,
+       currentUser: firstUser
+   };
 }
 
 class Users extends React.Component {
-    constructor() {
+  constructor() {
     super();
-    console.log('blablabla xxxxx')
     this.state = getStartState();
+    this.handleUserClick = this.handleUserClick.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
   }
 
-  handleNewGameClick(i) {
-    this.setState(getStartState());
+ handleUserClick(user) {
+     this.setState({currentUser: user});
+     console.log('current-user:',this.state.currentUser.firstName, this.state.currentUser.lastName);
   }
 
- handleClick(i) {
-    this.setState({});
+  handleFormChange(e) {
+      const changedUser = R.clone(this.state.currentUser);
+      changedUser[e.target.name] = e.target.value;
+      const users = this.state.users.map((user) => {
+          return user.userId === changedUser.userId ? changedUser : user;
+      });
+      this.setState({users: users,
+                     currentUser: changedUser});
   }
 
   render() {
@@ -113,9 +129,9 @@ class Users extends React.Component {
         <div>
         <div className="leftList" class="col-md-4">
             <UserHeader />
-            <UserList users={userList.users}/>
+            <UserList users={this.state.users} userClick={this.handleUserClick}/>
         </div>
-        <UserForm />
+        <UserForm user={this.state.currentUser} onChange={this.handleFormChange}/>
     </div>
     );
   }

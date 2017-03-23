4cba8f0c56006eb0c1629eff05bfa008e98f4eef
Todd Merrill <github@toddmerrill.com>
2017-03-23 22:34:27 +0100

    webpack config linter changes

--- webpack.config.js
+++ webpack.config.js
@@ -9,91 +9,91 @@ const rules = [
     loader: 'awesome-typescript-loader',
   },
   {
-  test: /\.js?$/,
-  exclude: /node_modules/,
-        use: [{
-            loader: 'babel-loader',
-            options: {
-                presets: [
-                    'react',
-                    'latest',
-                    'stage-3'
-                ],
-                plugins: []
-            }
-        },
-        {
-            loader: 'eslint-loader',
-            options: {
-                plugins: [ 'react' ]
-            }
-        }
-    ]
+    test: /\.js?$/,
+    exclude: /node_modules/,
+    use: [{
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          'react',
+          'latest',
+          'stage-3',
+        ],
+        plugins: [],
+      },
     },
     {
-        test: /\.css$/,
-        use: [
-            'style-loader',
-            'css-loader'
-        ]
+      loader: 'eslint-loader',
+      options: {
+        plugins: ['react'],
+      },
     },
-    {
-        test: /\.png$/,
-        loader: 'url-loader',
-        options: {
-            limit: '100000'
-        }
+    ],
+  },
+  {
+    test: /\.css$/,
+    use: [
+      'style-loader',
+      'css-loader',
+    ],
+  },
+  {
+    test: /\.png$/,
+    loader: 'url-loader',
+    options: {
+      limit: '100000',
     },
-    {
-        test: /\.jpg$/,
-        loader: "file-loader"
+  },
+  {
+    test: /\.jpg$/,
+    loader: 'file-loader',
+  },
+  {
+    test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+    loader: 'url-loader',
+    options: {
+      limit: '10000',
+      mimetype: 'application/font-woff',
     },
-    {
-        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader',
-        options: {
-            limit: '10000',
-            mimetype: 'application/font-woff'
-        }
+  },
+  {
+    test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+    loader: 'url-loader',
+    options: {
+      limit: '10000',
+      mimetype: 'application/octet-stream',
     },
-    {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader',
-        options: {
-            limit: '10000',
-            mimetype: 'application/octet-stream'
-        }
+  },
+  {
+    test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+    loader: 'file-loader',
+  },
+  {
+    test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+    loader: 'url-loader',
+    options: {
+      limit: '10000',
+      mimetype: 'image/svg+xml',
     },
-    {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader'
-    },
-    {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader',
-        options: {
-            limit: '10000',
-            mimetype: 'image/svg+xml'
-        }
-    }
+  },
 
 ];
 
 module.exports = {
-    devtool: 'eval-source-map',
-    entry: path.resolve('src', 'usersapp.js'),
-    output: {
-        path: path.resolve('build'),
-        filename: '[name].js',
-        publicPath: '/'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve('src', 'index.html'),
-            inject: 'body',
-            filename: 'index.html'
-        })
-    ],
+  devtool: 'eval-source-map',
+  entry: path.resolve('src', 'usersapp.js'),
+  output: {
+    path: path.resolve('build'),
+    filename: '[name].js',
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('src', 'index.html'),
+      inject: 'body',
+      filename: 'index.html',
+    }),
+  ],
   module: {
     rules,
   },
